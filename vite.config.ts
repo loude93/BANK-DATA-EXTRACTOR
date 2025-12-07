299a46f9bec22a504c4e4f82f2a6cd424a554f2d
CMohamed <chadad.mohamed@gmail.com>
2025-12-08 00:03:14 +0100

    Revert "Hchoma"

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify('AIzaSyByBgT5KFVFgqU-1Q5yH9hSwM5_KQApceY'),
-        'process.env.GEMINI_API_KEY': JSON.stringify('AIzaSyByBgT5KFVFgqU-1Q5yH9hSwM5_KQApceY')
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
